f7923ba8f1a28c2f56304211e4c1071b81678654
marom7 <marom7@gmail.com>
2023-12-31 03:18:50 +0200

    new account1.

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -12,34 +12,36 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Stack(
-        children: [
-          Image.asset('assets/login_page_bg.png'),
-          Padding(
-              padding: const EdgeInsets.only(bottom: 30.0, left: 30.0, right: 30.0),
-              child:Column(
-                children: [
-                  Text('Welcome Back!'),
-                  Text('Enter Your Username & Password'),
-                  Text('Username'),
-                  TextField(),
-                  Text('Password'),
-                  TextField(keyboardType: TextInputType.none),
-                  LoginButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/get_in');
-                    },
-                    text: 'Login',),
-                  TextButton(child:Text('Forgotten Password?'), onPressed: () {  }, ),
-                  TextButton(child:Text('Or Create a New Account'), onPressed: () {
-                    Navigator.pushNamed(context, '/create_account');
-                  },),
-                ],
-              ))
+        appBar: AppBar(),
+        body: Stack(
+            children: [
+              Image.asset('assets/login_page_bg.png'),
+                  Padding(
+                      padding: const EdgeInsets.only(bottom: 30.0, left: 30.0, right: 30.0),
+                      child:Column(
+                        children: [
+                          Text('Welcome Back!'),
+                          Text('Enter Your Username & Password'),
+                          Text('Username'),
+                          TextField(),
+                          Text('Password'),
+                          TextField(keyboardType: TextInputType.none),
+                          LoginButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/get_in');
+                            },
+                            text: 'Login',),
+                          TextButton(child:Text('Forgotten Password?'), onPressed: () {  }, ),
+                          TextButton(child:Text('Or Create a New Account'), onPressed: () {
+                            Navigator.pushNamed(context, '/create_account');
+                          },),
+                        ],
+                      ))
 
-        ],
-      ),
-    );
+
+
+            ])
+        );
+
   }
 }
