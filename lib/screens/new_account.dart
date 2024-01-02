14469b55f35ca4bcb2c49a3661c0a7547a32ff03
marom <marom7@gmail.com>
2024-01-02 13:57:34 +0200

    01-01-24

--- lib/screens/new_account.dart
+++ lib/screens/new_account.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/app_buttons.dart';
+
 class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
 
@@ -10,6 +12,45 @@ class CreateAccount extends StatefulWidget {
 class _CreateAccountState extends State<CreateAccount> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(),
+        body: Stack(
+            children: [
+              Align(alignment: AlignmentDirectional.bottomStart,
+                  child: Image.asset('assets/sign_up_bg.png')),
+              Padding(
+                  padding: const EdgeInsets.only(
+                      bottom: 25.0, left: 30.0, right: 30.0),
+                  child: Column(
+                      children: [
+                        Text('Create Account :)', style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold)),
+                        Spacer(),
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child: Text('Enter Email Id:', style: TextStyle(fontSize: 22),)),
+                        TextField(),
+                        //SizedBox(height: 25),
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child: Text('Create Username:', style: TextStyle(fontSize: 22),)),
+                        TextField(),
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child: Text('Create Password', style: TextStyle(fontSize: 22),)),
+                        TextField(),
+                        Spacer(),
+                        LoginButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/login');
+                          },
+                          text: 'Sign Up',),
+                      ]
+                  )
+              ),
+            ]
+        )
+    );
   }
 }
+
