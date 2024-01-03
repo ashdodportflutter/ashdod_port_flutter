c93e813cec47a357ef10cddc2cc0defcd575c8a2
marom7 <marom7@gmail.com>
2024-01-03 02:09:08 +0200

    done.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -14,35 +16,76 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-           body:  inputDate(),
+           body: inputDate(),
+           //endDrawer: gridCards(4) ,
+
+                   //inputDate(),
+
+
+
     );
+
+
   }
 
-  inputDate() {
+  Padding inputDate() {
     return
       Padding(
-        padding: const EdgeInsets.only(top: 80, right: 30, left: 30),
-        child: TextField(
-          decoration: const InputDecoration(
-            labelText: 'DATE',
-            filled: true,
-            prefixIcon: Icon(Icons.calendar_today),
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide.none
-            ),
-            focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blue)
-            ),
-          ),
-          controller: _dateController,
-          readOnly: true,
-          onTap:() {
-            _selectDate(context);
-          },
-        ),
-      );
+        padding: const EdgeInsets.only(top: 50, right: 30, left: 30),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
 
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'DATE',
+                filled: true,
+                prefixIcon: Icon(Icons.calendar_today),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue)
+                ),
+              ),
+              controller: _dateController,
+              readOnly: true,
+              onTap:() {
+                _selectDate(context);
+              },
+            ),
+            Expanded(
+              child: gridCards(4),
+
+            )
+          ],
+        )
+
+
+      );
   }
+
+  GridView gridCards(int _count) {
+    return GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20.0),
+        crossAxisSpacing: 10.0,
+        crossAxisCount: 2,
+        children:
+        List<Widget>.generate(
+          _count,
+              (int index) {
+            return GridTile(
+              child: Card(
+                color:
+                Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+                    .withOpacity(1.0),
+              ),
+            );
+          },
+        ));
+  }
+
 
   Future<void> _selectDate(BuildContext context) async{
      DateTime? _picked = await showDatePicker(
